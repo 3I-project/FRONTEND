18e0283d44413b8de873d95e2f43250c472da277
TankistPro <nakol.nayk@mail.ru>
2021-12-16 14:49:01 +0300

    added comment system

--- src/views/IdeaView/IdeaView.vue
+++ src/views/IdeaView/IdeaView.vue
@@ -13,36 +13,31 @@
       <div class="idea-comments">
         <div class="input-comment">
           <p>Оставить комментарий</p>
-          <textarea placeholder="Написать комментарий..."></textarea>
-          <MyButton class="comment-input blue-btn">Отправить</MyButton>
-          <span>2 комментария</span>
+          <textarea v-model="commentInput" placeholder="Написать комментарий..."></textarea>
+          <MyButton class="comment-input blue-btn" @click="sendComment">Отправить</MyButton>
+          <span>{{ comments.length }} комментария</span>
         </div>
         <div class="comment-line"></div>
-        <div class="comments-wrapper">
-          <div class="comment-item">
+        <div class="comments-wrapper" v-if="comments.length">
+          <div class="comment-item" v-for="comment in comments" :key="comment.id_comment">
             <div class="comment-item__top">
               <div class="user-avatar">
-                <img src="https://avatars.dicebear.com/api/avataaars/Tank.svg" alt="">
+                <img :src="`https://avatars.dicebear.com/api/avataaars/${ comment.author.first_name }.svg`" alt="">
               </div>
-              <p>Андрей Николаевич</p>
-              <small>2 часа назад</small>
+              <p>
+                {{ comment.author.first_name }} {{ comment.author.last_name }}
+
+                <span v-if="comment.author.isLeader">(руководитель организации)</span>
+              </p>
+              <small>{{ comment.created_date }} назад</small>
             </div>
             <div class="comment-item__content">
-              <p>Собираем команду разработиков 05.06, обсуждаем этот проект и приступаем к работе!</p>
+              <p>{{ comment.text }}</p>
             </div>
           </div>
-          <div class="comment-item">
-            <div class="comment-item__top">
-              <div class="user-avatar">
-                <img src="https://avatars.dicebear.com/api/avataaars/Tank.svg" alt="">
-              </div>
-              <p>Андрей Николаевич</p>
-              <small>2 часа назад</small>
-            </div>
-            <div class="comment-item__content">
-              <p>Собираем команду разработиков 05.06, обсуждаем этот проект и приступаем к работе!</p>
-            </div>
-          </div>
+        </div>
+        <div class="comments-empty" v-else>
+          <p>Будь первый! Напиши комментарий!</p>
         </div>
       </div>
     </template>
@@ -51,6 +46,8 @@
 
 <script>
 import './ideaView.scss';
+
+import { mapGetters } from 'vuex'
 
 import MyButton from "../../components/UI/MyButton/MyButton";
 
@@ -64,6 +61,8 @@ export default {
     return {
       isLoading: true,
       ideaData: null,
+      comments: 0,
+      commentInput: '',
     }
   },
   components: {
@@ -72,27 +71,76 @@ export default {
     VoteController,
     MyButton
   },
-  mounted() {
+  computed: {
+    ...mapGetters(['userProfile'])
+  },
+  methods: {
+    async sendComment () {
+      const requestPayload = {
+        text: this.commentInput,
+        id_idea: this.$route.params.id
+      }
+
+      await this.$api.post('/comments/create', requestPayload,{
+        headers: {
+          Authorization: localStorage.getItem('access')
+        }
+      })
+      .then(async response => {
+        const { data } = response;
+
+
+
+        if (data.status) {
+          await this.getIdeaComments();
+
+          this.commentInput = ''
+        }
+      })
+      .catch(error => {
+        console.log(error.response);
+      })
+    },
+    async getIdeaComments() {
+      await this.$api.get(`/comments?id=${this.$route.params.id}`, {
+        headers: {
+          Authorization: localStorage.getItem('access')
+        }
+      })
+      .then(response => {
+        const { data } = response;
+
+        console.log(data);
+
+        this.comments = data.comments;
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
+    },
+    async getIdeaData () {
+      await this.$api.get(`/idea/idea?id=${this.$route.params.id}`, {
+        headers: {
+          Authorization: localStorage.getItem('access')
+        }
+      })
+          .then(response => {
+            const { data } = response;
+
+            this.ideaData = data.idea;
+          })
+          .catch(error => {
+            console.log(error.response)
+          })
+    }
+  },
+  async mounted() {
     this.isLoading= true;
 
-    this.$api.get(`/idea/idea?id=${this.$route.params.id}`, {
-      headers: {
-        Authorization: localStorage.getItem('access')
-      }
-    })
-    .then(response => {
-      const { data } = response;
+    await this.getIdeaData();
+    await this.getIdeaComments();
 
-      this.ideaData = data.idea;
-
-      console.log(data)
-    })
-    .catch(error => {
-      console.log(error.response)
-    })
-    .finally(() => {
-      this.isLoading = false
-    })
+    this.isLoading = false
   }
 }
 </script>
